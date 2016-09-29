a0d94fdbeefa0a07936e25d16eecb3007aa0e346
DavoLarris <davo.larris@hotmail.com>
2016-09-29 12:18:26 +0200

    Everything done

--- app/src/main/java/org/cuatrovientos/davolarris/imc/MenuActivity.java
+++ app/src/main/java/org/cuatrovientos/davolarris/imc/MenuActivity.java
@@ -1,7 +1,10 @@
 package org.cuatrovientos.davolarris.imc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +12,31 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Button b1 = (Button) findViewById(R.id.button);
+
+        Button b2 = (Button) findViewById(R.id.button2);
+
+        Button b3 = (Button) findViewById(R.id.button3);
+
+    }
+
+    public void OnClick(View v) {
+        Intent intent;
+        // Perform action on click
+        switch (v.getId()) {
+            case R.id.button:
+                intent = new Intent(MenuActivity.this, IMCActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.button2:
+                intent = new Intent(MenuActivity.this, HelpActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.button3:
+                intent = new Intent(MenuActivity.this, AboutActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
